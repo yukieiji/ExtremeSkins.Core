fe3dfd8e459fda518036c2d4ba4f7a308f6c7836
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-21 22:42:55 +0900

    feat:DataStruct

--- ExtremeSkins.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExtremeSkins.Core;
-
-public class Class1
-{
-
-}
---
+++ ExtremeSkins.Core/ExtremeNamePlate/DataStructure.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace ExtremeSkins.Core.ExtremeNamePlate;
+
+public static class DataStructure
+{
+    public static string GetNamePlatePath(string hatParentPath, string namePlateName)
+        => Path.Combine(hatParentPath, $"{namePlateName}.png");
+}
