7df481bfb8c62280fdab5e240eec0d88eaf13e7f
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-25 20:34:54 +0900

    fix:missing

--- ExtremeSkins.Core/ExtremeNamePlate/DataStructure.cs
+++ ExtremeSkins.Core/ExtremeNamePlate/DataStructure.cs
@@ -5,6 +5,6 @@ namespace ExtremeSkins.Core.ExtremeNamePlate;
 public static class DataStructure
 {
     public const string FolderName = "ExtremeNamePlate";
-    public static string GetNamePlatePath(string hatParentPath, string namePlateName)
-        => Path.Combine(hatParentPath, $"{namePlateName}.png");
+    public static string GetNamePlatePath(string npParentPath, string autherName, string namePlateName)
+        => Path.Combine(npParentPath, autherName, $"{namePlateName}.png");
 }
